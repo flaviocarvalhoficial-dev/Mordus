5a0431b82db155e087c46656e13eefe968b74058
PinGoo-AppWeb <app.pingoo@gmail.com>
2026-04-08 23:09:31 -0300

    remoção barra verde

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.v2dp451ig6o"
+    "revision": "0.v0ibs89ul3o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
